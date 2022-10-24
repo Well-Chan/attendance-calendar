6784c5974536c0104eb64c8dbdfe481e78b5bf15
Well-Chan <1465561662@qq.com>
2022-10-24 22:48:21 +0800

    chore: 修改config文件，把构建出的文件夹改为docs，也推到远程仓库里用于发布

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': path.join(__dirname, './src')
